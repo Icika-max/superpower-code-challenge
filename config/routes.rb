c3ea2ead4a8e1edac91b5f4e0a10c99806f72a46
icika-max <icikakennedy@gmail.com>
2023-03-27 07:47:23 +0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :heroes, only: [:index, :show]
+  resources :heros, only: [:index, :show]
   resources :powers, only: [:index, :show, :update]
   resources :hero_powers, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
